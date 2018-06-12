dcaae32d4c51d22be9cb04cc444791a7d97c3277
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-06-11 21:19:51 -0700

    Finished reverseString

--- html_css/javascript-exercises/reverseString/reverseString.js
+++ html_css/javascript-exercises/reverseString/reverseString.js
@@ -1,5 +1,10 @@
-var reverseString = function() {
-
-}
+var reverseString = function(string) {
+    array = string.split("");
+    reversedArray = []
+    for(let i = 0; i < array.length; i++) {
+      reversedArray.unshift(array[i]);
+    }
+    return reversedArray.join('');
+  }
 
 module.exports = reverseString
