1dde8b9252b453bfb1ebc5b92012689b134e05e0
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-06-12 11:10:35 -0700

    Finished leap years

--- html_css/javascript-exercises/leapYears/leapYears.js
+++ html_css/javascript-exercises/leapYears/leapYears.js
@@ -1,5 +1,13 @@
-var leapYears = function() {
-
+var leapYears = function(year) {
+  if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  } else {
+    if (year % 4 === 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
 
 module.exports = leapYears
