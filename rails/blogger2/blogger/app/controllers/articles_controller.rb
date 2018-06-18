8f6e7c6ada42a0f0e50ae4892a7a60f5660933d1
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-06-17 23:55:30 -0700

    Refactored application controller, create show page

--- rails/blogger2/blogger/app/controllers/articles_controller.rb
+++ rails/blogger2/blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -6,4 +7,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find_by(id: params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+
 end
