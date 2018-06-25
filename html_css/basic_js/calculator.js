4c4984096015385f6a47ab41ebfcc0f66b10cb21
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-06-24 22:33:28 -0700

    Practe small calculator

---
+++ html_css/basic_js/calculator.js
@@ -0,0 +1,22 @@
+
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.
+
+
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+
+var first = prompt("Enter First Number");
+
+var second = prompt("Enter Second Number");
+
+var sum = alert(Number(first) + Number(second));
+
+var subtract = alert(first - second);
+
+var multiply = alert(first * second);
+
+var divide = alert(first / second);
