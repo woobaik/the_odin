e185f8a7ad07591b7f5264a1c9c6ca1eab8f8258
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-02 14:58:54 -0700

    Clear all preset components

--- react/modern_react/src/index.js
+++ react/modern_react/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const App = () => {
+  return (<h1>Hello World</h1>)
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
