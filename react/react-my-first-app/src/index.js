de517815b793afa5d118da9113adfd549e9f871d
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-03 03:41:48 -0700

    Implement database

--- react/react-my-first-app/src/index.js
+++ react/react-my-first-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
-import { robots } from './robots.js';
+import CardList from './CardList'
+import { robots } from './robots'
 // import registerServiceWorker from './registerServiceWorker';
 
 
 
 
-ReactDOM.render(< Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />,
+ReactDOM.render(<CardList robots={robots}/>,
   document.getElementById('root'));
