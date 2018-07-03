311919d82ff3add9f6c29f32e90581acbe274432
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-02 21:06:32 -0700

    Modify css

--- react/modern_react/src/components/header.js
+++ react/modern_react/src/components/header.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 
-const Header = () => {
-  return (<div>Header!</div>)
-}
+
+class Header extends Component {
+
+
+  inputChange(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <header>
+        <div className="logo">Logo</div>
+        <input onChange={this.inputChange} placeholder="He Lives in You"/>
+
+      </header>
+    );
+  }
+};
 
 export default Header;
--- react/modern_react/src/index.js
+++ react/modern_react/src/index.js
@@ -8,7 +8,6 @@ import Header from './components/header';
 const App = () => {
   return (
     <div>
-      <h1>Hello!</h1>
       <Header />
     </div>)
 }
