212c4b044a7c1e6f1b45ff68daf42c46ec9522b6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-03 02:52:52 -0700

    create sample cards

--- react/react-my-first-app/src/Card.js
+++ react/react-my-first-app/src/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
       <h1>Robo Friend</h1>
-      <img alt="card-pic" src='https://robohash.org/test?200x200'/>
+      <img alt="card-pic" src={`https://robohash.org/${props.id}?200x200`} />
 
       <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
--- react/react-my-first-app/src/index.js
+++ react/react-my-first-app/src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import { robots } from './robots.js';
 // import registerServiceWorker from './registerServiceWorker';
 
 
-const Welcome = ((props) => {
-  return(
-  <h4>Welcome to my site! {props.firstName}!</h4>
-  )
-});
 
 
-
-ReactDOM.render(< Card />,
+ReactDOM.render(< Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />,
   document.getElementById('root'));
