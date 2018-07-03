45530cde80b4a0396670cb4140cd3781e8aaf2cf
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-03 01:39:57 -0700

    cs work for list item

--- react/modern_react/src/components/news_list_item.js
+++ react/modern_react/src/components/news_list_item.js
@@ -4,7 +4,7 @@ import React from 'react';
 const newsItem = (props) => {
   console.log(props);
   return (
-    <div>
+    <div className='news_item'>
       <h3>{props.item.title}</h3>
       <div>
         {props.item.feed}
