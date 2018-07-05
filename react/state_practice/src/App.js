402ea33b6a08a371585b6b38477bc67df58a9cab
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-04 23:25:23 -0700

    Create Layout component

--- react/state_practice/src/App.js
+++ react/state_practice/src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './components/Layout.js'
 
 class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout />
+      </div>
+    )
+  }
 
 }
 
