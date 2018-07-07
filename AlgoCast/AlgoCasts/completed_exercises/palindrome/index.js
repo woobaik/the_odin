7d9d83953abc61de2a8d1b0565e0bc4700774ed6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-06 23:33:41 -0700

    Finish palindrome

--- AlgoCast/AlgoCasts/completed_exercises/palindrome/index.js
+++ AlgoCast/AlgoCasts/completed_exercises/palindrome/index.js
@@ -8,9 +8,8 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  return str.split('').every((char, i) => {
-    return char === str[str.length - i - 1];
-  });
+  return str.split('').every((char, i) =>
+    char === str[str.length - i - 1])    
 }
 
 module.exports = palindrome;
