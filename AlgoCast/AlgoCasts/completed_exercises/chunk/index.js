dc1b365f5aaa47a57c08f87e0900591192d388cd
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-08 22:05:40 -0700

    Finish chunk

--- AlgoCast/AlgoCasts/completed_exercises/chunk/index.js
+++ AlgoCast/AlgoCasts/completed_exercises/chunk/index.js
@@ -8,20 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-
 function chunk(array, size) {
-  const result = []
+  const chunked = [];
+  let index = 0;
 
-  for (let element of array) {
-    let last = result[result.length - 1]
-
-    if (!last || last.length === size) {
-     result.push([element])
-    } else {
-      last.push(element)
-    }
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
-  return result
+
+  return chunked;
 }
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   const chunked = [];
+//
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//
+//   return chunked;
+// }
