45c7e961fbb6e92f62a83aabe84f401d9e452261
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-11 20:54:47 -0700

    Create lecture CRUD

--- rails/ratemyprofessor/app/controllers/lectures_controller.rb
+++ rails/ratemyprofessor/app/controllers/lectures_controller.rb
@@ -1,4 +1,24 @@
 class LecturesController < ApplicationController
   def index
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end 
+
 end
