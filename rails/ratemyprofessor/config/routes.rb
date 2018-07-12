1babaf34e326dd0e01024d1ffca23cd9df182860
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-11 20:55:46 -0700

    Change lecture routes to resources

--- rails/ratemyprofessor/config/routes.rb
+++ rails/ratemyprofessor/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'lectures/index'
+  resources :lectures
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
