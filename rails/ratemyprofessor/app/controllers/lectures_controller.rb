fe66136b164e320002395ca23c25ac27f0fb69bf
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-13 23:19:18 -0700

    Create new create action

--- rails/ratemyprofessor/app/controllers/lectures_controller.rb
+++ rails/ratemyprofessor/app/controllers/lectures_controller.rb
@@ -5,15 +5,22 @@ class LecturesController < ApplicationController
   end
 
   def new
+    @lecture = Lecture.new
 
   end
 
   def create
-
+    @lecture = Lecture.new(lecture_params)
+    if @lecture.save
+      flash[:notice] = 'Your article is successfully added'
+      redirect_to lecture_path
+    else
+      flash.now[:notice] = 'Ooops, there was a problem creating lecture list'
+      render :lecture => :new
   end
 
   def edit
-
+    @lecture = Lecture.find_by(id: params[:id])
   end
 
   def update
@@ -23,4 +30,10 @@ class LecturesController < ApplicationController
   def destroy
   end
 
+
+  private
+
+  def lecture_params
+    params.require(:lecture).permit(:lecture_name, :semester, :credits)
+  end
 end
