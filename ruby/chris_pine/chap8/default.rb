238326728df29ea7e2816e10ad1355d4b04f4b93
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-09-22 23:36:45 -0700

    origin master

--- ruby/chris_pine/chap8/default.rb
+++ ruby/chris_pine/chap8/default.rb
@@ -23,3 +23,5 @@ haram = Girl.new("haram", "good")
 p haram.name
 haram.size = "very good"
 p haram.size
+# in order to access instance variable, we should make a
+#getter or setter methods. (attr_accessor)
