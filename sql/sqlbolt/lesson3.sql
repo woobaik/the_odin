b099a6bd032987061c8539593a9c9a3e8a0a9de0
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-01 08:26:58 -0700

    Finish lesson 3

---
+++ sql/sqlbolt/lesson3.sql
@@ -0,0 +1,7 @@
+SELECT * FROM movies WHERE title LIKE "%Toy Story%"
+
+SELECT * FROM movies WHERE director = "John Lasseter"
+
+SELECT * FROM movies WHERE director != "John Lasseter"
+
+SELECT * FROM movies WHERE title LIKE "WALL-_"
