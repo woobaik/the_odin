32e984fb19b0ea6a58e48ec5664dabaf44d7a0ef
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-02 19:24:37 -0700

    Finish sinatra 101

--- sinatra/web_guesser/web_guesser.rb
+++ sinatra/web_guesser/web_guesser.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  guess = params["guess"]
   number = rand(100)
   erb :index, :locals => {:number => number}
+  throw params.inspect
+
 end
