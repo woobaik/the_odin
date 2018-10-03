a9b44583167fcd08c2ad135b0d38f69ab66cc48e
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-03 15:21:23 -0700

    Initialize NEW, SHOW, ROUTES, CSS

---
+++ rails/blogger2/brogger/app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+end
