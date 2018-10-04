b654f0b8ce335d6f686d0fac5ae9199d33f6702b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-03 20:14:09 -0700

    Finish blog comments feature

--- rails/blogger2/brogger/app/controllers/articles_controller.rb
+++ rails/blogger2/brogger/app/controllers/articles_controller.rb
@@ -18,6 +18,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id 
   end
 
   def edit
