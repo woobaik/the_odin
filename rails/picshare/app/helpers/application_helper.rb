0ab6579bd24fd194ca3a33bc528d02504de0715a
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-05 22:11:40 -0700

    Set App, Users & sessions

--- rails/picshare/app/helpers/application_helper.rb
+++ rails/picshare/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  private
+  def current_user
+    session[:user_id] && User.find(session[:user_id])
+  end
 end
--- rails/picshare/config/routes.rb
+++ rails/picshare/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  get 'sessions', to: 'sessions#new'
 end
