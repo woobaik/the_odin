0c820b2f0c90e817d0619bf130e9b8c0afd78fb9
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-10 23:38:56 -0700

    Finish constraints

--- rails/routes_demo/config/routes.rb
+++ rails/routes_demo/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  with_options only: [:index, :show] do |list|
-    list.resources :human
-    list.resources :zomibe
-    list.resources :medical_kit
+  resources :human
+  constraints subdomain: 'api' do
+    resources :zombies
+    resources :medical
   end
 end
