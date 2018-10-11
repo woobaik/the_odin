4fcf88fcb774302b4ac1a2267114e8757d5ec7a6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-11 14:34:18 -0700

    remove all routes for new practice

--- rails/routes_demo/config/routes.rb
+++ rails/routes_demo/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :human
-  constraints subdomain: 'api' do
-    resources :zombies
-    resources :medical
-  end
+
 end
