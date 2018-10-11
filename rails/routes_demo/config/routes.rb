3a7d202610b72f814a81c06bdf1e091d8cab1ee6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-10 22:53:15 -0700

    Basic resources methods

--- rails/routes_demo/config/routes.rb
+++ rails/routes_demo/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :zombies, except: [:destroy]
+  resources :humans, only: [:index, :show]
 end
