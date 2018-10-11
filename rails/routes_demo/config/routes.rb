3fc349486edd4c651a5156fb8a821494ceec132b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-10 23:31:49 -0700

    Used with_options for DRY

--- rails/routes_demo/config/routes.rb
+++ rails/routes_demo/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :zombies, except: [:destroy]
-  resources :humans, only: [:index, :show]
+  with_options only: [:index, :show] do |list|
+    list.resources :human
+    list.resources :zomibe
+    list.resources :medical_kit
+  end
 end
