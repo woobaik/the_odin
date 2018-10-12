c30f3f47d08b5218d40346c1dca9e7e480f9dc18
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-11 20:37:15 -0700

    Practice scope in model with SQL QUERY

--- rails/routes_demo/app/models/subject.rb
+++ rails/routes_demo/app/models/subject.rb
@@ -1,3 +1,9 @@
 class Subject < ApplicationRecord
   has_many :pages
+
+  scope :visible, lambda { where(visible: true)}
+  scope :invisible, lambda { where(visibie: false)}
+  scope :sorted, lambda { order("position ASC")}
+  scope :newest_first, lambda { order("position DESC")}
+  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
