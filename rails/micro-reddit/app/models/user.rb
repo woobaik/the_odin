d5ba6ca902b4e6e91cdf754a2b6fc9c54956e0bb
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-20 11:17:34 -0700

    Implement Post and User association

--- rails/micro-reddit/app/models/user.rb
+++ rails/micro-reddit/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :username, presence:true, length: { in: 6..12}, uniqueness: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Please enter vaild e-mail address"}
 end
