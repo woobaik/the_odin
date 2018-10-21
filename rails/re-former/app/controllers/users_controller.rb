6b766f478a6b44570c1f2d7597d9cdcd276913e7
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-21 14:41:13 -0700

    Finish part 3

--- rails/re-former/app/controllers/users_controller.rb
+++ rails/re-former/app/controllers/users_controller.rb
@@ -12,6 +12,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
