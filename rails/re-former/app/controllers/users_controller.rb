5d8204953680cae60509e01461c6d93901906215
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-21 14:10:52 -0700

    Finish part 2

--- rails/re-former/app/controllers/users_controller.rb
+++ rails/re-former/app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    # create_table "users", force: :cascade do |t|
+    #   t.string "username"
+    #   t.string "email"
+    #   t.string "password_digest"
+    params.require(:user).permit(:username, :email, :password)
   end
 end
