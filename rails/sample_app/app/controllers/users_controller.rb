036eb68881ff09b0b10e7fed5a1deec24f8a3b6d
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-23 00:38:49 -0700

    Finish chapter 8

--- rails/sample_app/app/controllers/users_controller.rb
+++ rails/sample_app/app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       flash[:success] = "Thanks for Sign Up"
       redirect_to user_path(@user)
     else
--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   root to: 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   resources :users
 end
