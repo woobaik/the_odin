93a49184a2acab472fb1e9c64236c02afa6d2417
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-23 18:21:55 -0700

    Implement current user via session

--- rails/sample_app/app/models/user.rb
+++ rails/sample_app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {self.email = self.email.downcase}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -8,4 +9,21 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.digest(string)
+    BCrypt::Password.create(string)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?
+    BCrypt::Password.new(remember_diest).is_password?(remember_token)
+  end
 end
