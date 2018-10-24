e66b5d41dc939c3e5c3a962e8c3c58e777cd7167
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-24 16:06:54 -0700

    Implement login function

--- rails/99cats/app/controllers/sessions_controller.rb
+++ rails/99cats/app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :redirect, only: [:new]
   def new
 
   end
@@ -9,8 +10,18 @@ class SessionsController < ApplicationController
       render json: 'Credential were wrong'
     else
     user.reset_session_token
+    login!(user)
     flash[:success] = "Welcome back #{user.username}!"
     redirect_to cats_path
     end
   end
+
+  def destroy
+    current_user.reset_session_token if current_user
+    session[:session_token] = nil
+    current_user = nil
+    redirect_to new_session_path
+  end
+
+
 end
--- rails/99cats/app/controllers/users_controller.rb
+++ rails/99cats/app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
 
+  before_action :redirect, only: [:new]
+  
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to root_path
+    else
+      flash[:fail] = "Failed to Sign Up"
+      render 'new'
+    end
   end
 
 
   private
   def user_params
-    params.require(:user).permit(:username, :password_digest)
+    params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
