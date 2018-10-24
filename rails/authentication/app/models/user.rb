ffbe10db8cd85368f3df2b854433cfc5ea1c8ba3
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-24 00:55:23 -0700

    Refactor minor errors

--- rails/authentication/app/models/user.rb
+++ rails/authentication/app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
   def self.find_by_credentials(username,password)
     user = User.find_by(username: username)
     return nil if user.nil?
-    user.is_password?
+    user.is_password?(password) ? user : nil
   end
 
   private
