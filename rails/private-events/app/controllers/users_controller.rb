6db1a2880b729bfd42c8de7e9997b09168f2572d
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-28 21:54:01 -0700

    Implemented vanilla user controller view and model

---
+++ rails/private-events/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Thank you for sign up"
+      redirect_to user_path(@user)
+    else
+      flash.now[:warning] = "Oops! something wrong with signing up!"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+end
