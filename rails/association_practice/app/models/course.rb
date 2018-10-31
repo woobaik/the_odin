1cdb48bda98fdf388415dc402c6f1eb0c8e62c7b
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-30 19:27:07 -0700

    Finish practice

--- rails/association_practice/app/models/course.rb
+++ rails/association_practice/app/models/course.rb
@@ -1,2 +1,18 @@
 class Course < ApplicationRecord
+  has_many :enrollments,
+    foreign_key: :course_id,
+    class_name: "Enrollment"
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    foreign_key: :course_id,
+    source: :user
+
+  has_many :prerequisite,
+    foreign_key: :prereq_id,
+    class_name: "Course"
+
+  belongs_to :instructor,
+    foreign_key: :instructor_id,
+    class_name: "User"
 end
