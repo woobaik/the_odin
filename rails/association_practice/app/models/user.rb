bf6bbc84df8eb3b6445197fb39d777a2ca3f644e
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-30 18:08:07 -0700

    Finish User association

--- rails/association_practice/app/models/user.rb
+++ rails/association_practice/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :enrollments,
+    foreign_key: :student_id,
+    class_name: "Enrollment"
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    foreign_key: :student_id,
+    source: :course
 end
