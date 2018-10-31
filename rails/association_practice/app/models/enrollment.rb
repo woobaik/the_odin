b2a593ab3fb1af7e8b46aa11ea74b035cd1adeec
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-10-30 17:47:44 -0700

    Finish associate enrollment model

--- rails/association_practice/app/models/enrollment.rb
+++ rails/association_practice/app/models/enrollment.rb
@@ -1,2 +1,9 @@
 class Enrollment < ApplicationRecord
+  belongs_to :user,
+    foreign_key: :student_id,
+    class_name: "User"
+
+  belongs_to :course,
+    foreign_key: :course_id,
+    class_name: "Course"
 end
