f677174733c4e8f8cb4db042ed8e3e24f60bf114
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-25 23:46:48 -0800

    Finish chap4

--- javascript/oop/hangman.js
+++ javascript/oop/hangman.js
@@ -2,12 +2,29 @@
 //2. Setup two attributes. One for the word itself. Another for the number of guesses allowed.
 
 const Hangman = function(word, numOfGuesses) {
-  this.word = word
+  this.word = word.toLowerCase().split('')
   this.numOfGuesses = numOfGuesses
+  this.guessLetters = ['c']
 }
 
-const hang1 = new Hangman('Poker Face', 3)
-console.log(hang1);
+
+Hangman.prototype.getPuzzle = function() {
+  let puzzle = ''
+  this.word.forEach((letter) => {
+    if (this.guessLetters.includes(letter) || letter === ' ') {
+      puzzle += letter
+    } else {
+      puzzle += '*'
+    }
+  })
+  return puzzle
+}
+
+
+const hang1 = new Hangman('Pocker Face  ', 3)
+console.log(hang1.getPuzzle());
 
 const hang2 = new Hangman('California', 5)
-console.log(hang2);
+
+
+/// Set up the word instance property as an array of lower case letter
